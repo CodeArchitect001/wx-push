e4e3285bd93f849fcbc2766267a4cc236cfa01da
CodeArchitect001 <164287215+CodeArchitect001@users.noreply.github.com>
2025-06-01 21:18:59 +0800

    Update index.js
    改了touser

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wx8d048dad0287f83c",
   appsecret: "c8030a25a157cd46bb77af2e6868aec0",
   template_id: "uilsrBVZKEfs7Lup-Sy1_e-UzK85utxBqGqHwDngmPQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  touser: "omFbB7Z73BQbLHcfBm2lwmRCdY8A",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "曼谷",
